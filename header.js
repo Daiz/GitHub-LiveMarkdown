b6826be7c6f25c63b3ef0a9042365109464404f4
Daiz <taneli.vatanen@gmail.com>
2013-05-18 19:36:22 +0300

    Add userscript header

---
+++ header.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name           Live Markdown Preview for GitHub
+// @namespace      daiz
+// @author         Daiz <taneli.vatanen@gmail.com>
+// @version        VERSION
+// @description    Preview your comments/issues/etc live on GitHub.
+// @include        https://github.com/*
+// @updateURL      https://github.com/Daiz-/GH-LiveMarkdown/raw/master/build/script.user.js
+// @downloadURL    https://github.com/Daiz-/GH-LiveMarkdown/raw/master/build/script.user.js
+// @run-at         document-end
+// ==/UserScript==
