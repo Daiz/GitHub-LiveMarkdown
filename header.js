cd6b1f08d08e32f9c7e12511932e5a9ed4fb649e
Daiz <taneli.vatanen@gmail.com>
2013-05-18 22:35:48 +0300

    Update userscript header

--- header.js
+++ header.js
@@ -5,7 +5,7 @@
 // @version        VERSION
 // @description    Preview your comments/issues/etc live on GitHub.
 // @include        https://github.com/*
-// @updateURL      https://github.com/Daiz-/GH-LiveMarkdown/raw/master/build/script.user.js
-// @downloadURL    https://github.com/Daiz-/GH-LiveMarkdown/raw/master/build/script.user.js
+// @updateURL      https://github.com/Daiz-/GitHub-LiveMarkdown/raw/master/build/script.user.js
+// @downloadURL    https://github.com/Daiz-/GitHub-LiveMarkdown/raw/master/build/script.user.js
 // @run-at         document-end
 // ==/UserScript==
