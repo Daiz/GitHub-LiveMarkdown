e668235945d41fb73ecc221810b58991a96f6e2a
Daiz <taneli.vatanen@gmail.com>
2013-05-19 12:52:08 +0300

    Update header @includes

--- src/header.js
+++ src/header.js
@@ -4,7 +4,10 @@
 // @author         Daiz <taneli.vatanen@gmail.com>
 // @version        VERSION
 // @description    Preview your comments/issues/etc live on GitHub.
-// @include        https://github.com/*
+// @include        https://github.com/*/*/issues/*
+// @include        https://github.com/*/*/commit/*
+// @include        https://github.com/*/*/pull/*
+// @include        https://github.com/*/*/pull/*/files
 // @updateURL      https://github.com/Daiz-/GitHub-LiveMarkdown/raw/master/build/script.user.js
 // @downloadURL    https://github.com/Daiz-/GitHub-LiveMarkdown/raw/master/build/script.user.js
 // @run-at         document-end
